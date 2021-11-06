4da0f5b75437c15ba149180e1d1460c5034ca31f
Eugene Egorov <genew@bk.ru>
2021-11-06 14:09:14 +0500

    changes

--- activity/routes/api.php
+++ activity/routes/api.php
@@ -1,8 +1,6 @@
 <?php
 
-use App\Http\Controllers\StatController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Procedures\StatProcedure;
 
 Route::rpc('/stat', [StatProcedure::class]);
-Route::get('/stat', [StatController::class, "index"]);
